b1671bcef7e2e749754d029bec6f4ef15cbfa2c1
Thomas Preisner <mail@tpreisner.de>
2025-01-03 21:43:26 +0100

    Revert "Lock interrupts in touchscreen component"
    This reverts commit 9411173dd48e20f4e57f69700a92cec7e43b2607.

--- esphome/components/touchscreen/touchscreen.cpp
+++ esphome/components/touchscreen/touchscreen.cpp
@@ -8,7 +8,6 @@ namespace touchscreen {
 static const char *const TAG = "touchscreen";
 
 void TouchscreenInterrupt::gpio_intr(TouchscreenInterrupt *store) {
-  InterruptLock lock;
   ESP_LOGW(TAG, "TouchscreenInterrupt: triggered!");
   store->touched = true;
 }
